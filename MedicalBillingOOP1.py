f1fd3ee7f5580f6a42f6a67432d8881d2caa3c8f
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2020-09-16 00:09:16 +0200

    Add files via upload

---
+++ MedicalBillingOOP1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 22:59:32 2020
+
+@author: Alaisha Naidu
+Name: Medical Billing OOP
+"""
+         
+class Specialist:
+    def __init__(self, consultation, prescription, medical_tests, radiology_referral, blood_tests, medical_aid_balance):
+        self.consultation = consultation
+        self.prescription = prescription
+        self.medical_tests = medical_tests
+        self.radiology_referral = radiology_referral
+        self.blood_tests = blood_tests
+        self.medical_aid_balance = medical_aid_balance
+        
+    def MedicalAidClaim(self):
+        if self.consultation + self.prescription + self.medical_tests + self.radiology_referral + self.blood_tests <= self.medical_aid_balance:
+            self.medical_aid_balance = self.medical_aid_balance - (self.consultation + self.prescription + self.medical_tests + self.radiology_referral + self.blood_tests)
+            print("Remaining Medical Aid balance is:")
+            print(self.medical_aid_balance)
+        else:
+            self.medical_aid_balance = self.medical_aid_balance
+            print("Not enough Medical Aid Funds, current balance is:")
+            print(self.medical_aid_balance)
+
+class Cardiologist(Specialist):
+    pass
+
+class Dentist(Specialist):
+    pass
+
+class Neurologist(Specialist):
+    pass
+
+class Optometrist(Specialist):
+    pass
+
+class GeneralSurgeon(Specialist):
+    pass
+
+patient1 = Cardiologist(500, 126, 0, 0, 0, 7600)
+patient1.MedicalAidClaim()
