22f38a8131d86e4fe47fe86695e3ef4a95e3835d
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2020-10-03 00:05:41 +0200

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 23:31:04 2020
+
+@author: Alaisha Naidu
+Name: Class inheritance, Method Inheritance
+Creds: DataCamp
+"""
+
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+    
+    def withdraw(self, amount):
+        self.balance - amount
+        
+class SavingsAccount(BankAccount):
+    def __init__(self, balance):
+        BankAccount.__init__(self, balance)
+        
+    def withdraw(self, amount, fee):
+        new_balance = BankAccount.withdraw(self, amount - fee)
+        return new_balance
+    
+transact1 = SavingsAccount(1000)
+transact1.withdraw(400, 5)
+
+transact2 = BankAccount(1000)
+transact2.withdraw(400,5)   #Produces error here because no fee applicable in BankAccount class
