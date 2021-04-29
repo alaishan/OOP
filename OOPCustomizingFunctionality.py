89a4f5bf68c5705a03088b5382bc399872b58d2e
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-04-29 08:40:42 +0200

    Add files via upload

--- OOPCustomizingFunctionality.py
+++ OOPCustomizingFunctionality.py
@@ -22,6 +22,27 @@ class SavingsAccount(BankAccount):
         BankAccount.__init__(self, balance) #self in this case is a Savings Account but also a Bank Account
         #Add more functionality
         self.interest_rate = interest_rate
+    
+    #New functionality
+    def compute_interest(self, n_periods = 1):
+        return self.balance*((1+self.interest_rate)**n_periods-1)
+
+class CheckingAccount(BankAccount):
+    def __init__(self, balance, limit):
+        BankAccount.__init__(self, balance)
+        self.limit = limit
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount, fee=0):
+        if fee <= self.limit:
+            BankAccount.withdraw(self, amount + fee)
+        else:
+            BankAccount.withdraw(self, amount + self.limit)
+            
 
 acct = SavingsAccount(1000, 0.03)
 print(acct.interest_rate)
+
+check_acct = CheckingAccount(1000, 25)
+check_acct.withdraw(200, fee = 15)
+print(check_acct.balance)
