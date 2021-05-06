6aadc69f4c559c676c37152b3840b8a734e3818e
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-05-06 10:29:03 +0200

    Add files via upload

---
+++ OOPExceptions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 10:32:00 2021
+
+@author: Alaisha Naidu
+Name: Exceptions
+Creds: DataCamp
+
+"""
+
+class BalanceError(Exception): pass #create an empty class
+
+class Customer:
+    def __init__(self, name, balance):
+        if balance < 0:
+            raise BalanceError("Balance has to be non-negative.")
+        else: 
+            self.name, self.balance = name, balance
+
+customer = Customer("Lucy", -5)
+
+#Retruns customized error: 
+#BalanceError: Balance has to be non-negative.
